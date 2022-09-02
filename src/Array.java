bcb8248752f630bb65fffddf4d3806e9c31de3a2
Qi Qian <qiqian@Qis-MacBook-Pro.local>
2022-09-02 16:10:12 -0400

    add best buy and sell stock

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-
 public class Array {
     /*
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -18,5 +17,25 @@ public class Array {
 
         }
         return new int[]{0,0};
+    }
+    /*
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+     */
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buyPrice = prices[0];
+        for(int i = 0; i< prices.length; i++){
+            int sellPrice = prices[i];
+            buyPrice = Math.min(buyPrice, sellPrice);
+            if(sellPrice-buyPrice>profit){
+                profit=sellPrice-buyPrice;
+            }
+        }
+        return profit;
     }
 }
